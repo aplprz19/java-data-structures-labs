f3862515d8bb50d79385a0bdf7836fa3c298c19c
aplprz19 <aplprzlo21@gmail.com>
2026-02-27 19:31:48 -0800

    Update comments for Item.java

--- lab5-iterator-inventory/README.md/Item.java
+++ lab5-iterator-inventory/README.md/Item.java
@@ -1,14 +1,19 @@
 public class Item {
+
+    //name of item
     private String name;
 
+    // creates item and name
     public Item(String name) {
         this.name = name;
     }
 
+    // Lets Inventory read the item name when searching for it
     public String getName() {
         return name;
     }
 
+    //prints readable
     @Override
     public String toString() {
         return name;
